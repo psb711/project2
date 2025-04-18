8db115c57f57085bdacb9821fd80bb21d273c2a6
psb711 <711psb@naver.com>
2025-04-18 18:23:11 +0900

    20250418

---
+++ nonsan.js
@@ -0,0 +1,86 @@
+
+
+// 헴버거
+$(function () {
+   $("#close").click(function () {
+      $("#allmenu").css("display", "none")
+   });
+   $("img[alt='hamburger']").click(function () {
+      $("#allmenu").css("display", "block")
+   });
+   // 돋보기
+
+});
+document.addEventListener('DOMContentLoaded', function () {
+   const container = document.getElementById('zoomContainer');
+   const lens = document.getElementById('zoomLens');
+   const zoomedImg = document.getElementById('zoomedImage');
+   const baseImg = document.getElementById('baseImage');
+
+   const zoom = 2;
+
+   baseImg.addEventListener('load', () => {
+
+      zoomedImg.style.width = baseImg.width * zoom + 'px';
+      zoomedImg.style.height = baseImg.height * zoom + 'px';
+   });
+
+   container.addEventListener('mousemove', function (e) {
+      lens.style.display = 'block';
+
+      const rect = container.getBoundingClientRect();
+      const x = e.clientX - rect.left;
+      const y = e.clientY - rect.top;
+
+      const lensWidth = lens.offsetWidth;
+      const lensHeight = lens.offsetHeight;
+
+      let lensX = x - lensWidth / 2;
+      let lensY = y - lensHeight / 2;
+
+
+      lensX = Math.max(0, Math.min(lensX, container.offsetWidth - lensWidth));
+      lensY = Math.max(0, Math.min(lensY, container.offsetHeight - lensHeight));
+
+      lens.style.left = lensX + 'px';
+      lens.style.top = lensY + 'px';
+
+
+      zoomedImg.style.left = -lensX * zoom + 'px';
+      zoomedImg.style.top = -lensY * zoom + 'px';
+   });
+
+   container.addEventListener('mouseleave', () => {
+      lens.style.display = 'none';
+   });
+});
+
+$(function () {
+
+   var menuItems = $("nav ul li").not(":first-child");
+
+
+   var sections = $("#s1,#s2, #s3, #s4, #s5, #s6");
+
+   var gap = -80;
+   menuItems.click(function (e) {
+      e.preventDefault();
+
+      var index = $(this).index();
+      var sectionTop = sections.eq(index).offset().top - gap;
+
+      $("html, body").animate({ scrollTop: sectionTop }, 600);
+   });
+
+
+   $(window).scroll(function () {
+      var scrollY = $(this).scrollTop();
+
+      sections.each(function (i) {
+         if (scrollY >= $(this).offset().top - (gap + 10)) {
+            menuItems.removeClass("active");
+            menuItems.eq(i).addClass("active");
+         }
+      });
+   });
+});
